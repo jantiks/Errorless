40534de8eb6c8be6c1463ccb35d8ca513b802c34
jantiks <58886410+jantiks@users.noreply.github.com>
2023-01-08 17:51:21 +0100

    removed the delay

--- Sources/ErrorlesSDK/Private/CrashReporter/ErrorlessCrashReporter.swift
+++ Sources/ErrorlesSDK/Private/CrashReporter/ErrorlessCrashReporter.swift
@@ -9,7 +9,7 @@ import CrashReporter
 
 struct ErrorlessCrashReporter {
     func initalize() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5, execute: {
+        DispatchQueue.main.async {
             let config = PLCrashReporterConfig(signalHandlerType: .mach, symbolicationStrategy: [])
             guard let crashReporter = PLCrashReporter(configuration: config) else {
                 print("Could not create an instance of PLCrashReporter")
@@ -44,6 +44,6 @@ struct ErrorlessCrashReporter {
             
             // Purge the report.
             crashReporter.purgePendingCrashReport()
-        })
+        }
     }
 }
