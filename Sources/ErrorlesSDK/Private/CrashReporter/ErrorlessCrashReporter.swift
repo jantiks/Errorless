d4a14080898c7b16b374be9d10cec98fce5dca1e
jantiks <58886410+jantiks@users.noreply.github.com>
2023-01-08 17:38:24 +0100

    bringing back the implementation.

--- Sources/ErrorlesSDK/Private/CrashReporter/ErrorlessCrashReporter.swift
+++ Sources/ErrorlesSDK/Private/CrashReporter/ErrorlessCrashReporter.swift
@@ -9,39 +9,30 @@ import CrashReporter
 
 struct ErrorlessCrashReporter {
     func initalize() {
-        let config = PLCrashReporterConfig(signalHandlerType: .mach, symbolicationStrategy: [])
-        guard let crashReporter = PLCrashReporter(configuration: config) else {
-            print("Could not create an instance of PLCrashReporter")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+            guard let crashReporter = PLCrashReporter.shared() else {
+                return
+            }
+
+            if crashReporter.hasPendingCrashReport() {
+                handleCrashReport(crashReporter)
+            }
+
+            if !crashReporter.enable() {
+                print("Could not enable crash reporter")
+            }
+        })
+    }
+
+    func handleCrashReport(_ crashReporter: PLCrashReporter) {
+        guard let crashData = try? crashReporter.loadPendingCrashReportDataAndReturnError(), let report = try? PLCrashReport(data: crashData), !report.isKind(of: NSNull.classForCoder()) else {
+            crashReporter.purgePendingCrashReport()
             return
         }
-        
-        // Enable the Crash Reporter.
-        do {
-            try crashReporter.enableAndReturnError()
-        } catch let error {
-            print("Warning: Could not enable crash reporter: \(error)")
-        }
-        
-        // Try loading the crash report.
-        if crashReporter.hasPendingCrashReport() {
-            do {
-                let data = try crashReporter.loadPendingCrashReportDataAndReturnError()
-                
-                // Retrieving crash reporter data.
-                let report = try PLCrashReport(data: data)
-                
-                // We could send the report from here, but we'll just print out some debugging info instead.
-                if let text = PLCrashReportTextFormatter.stringValue(for: report, with: PLCrashReportTextFormatiOS) {
-                    print(text)
-                } else {
-                    print("CrashReporter: can't convert report to text")
-                }
-            } catch let error {
-                print("CrashReporter failed to load and parse with error: \(error)")
-            }
-        }
-        
-        // Purge the report.
+
+        let crash: NSString = PLCrashReportTextFormatter.stringValue(for: report, with: PLCrashReportTextFormatiOS)! as NSString
+        // process the crash report, send it to a server, log it, etc
+        print("CRASH REPORT:\n \(crash)")
         crashReporter.purgePendingCrashReport()
     }
 }
