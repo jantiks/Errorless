49b3feba6e19941099e230c4567ea2f3e3e46925
jantiks <58886410+jantiks@users.noreply.github.com>
2023-01-09 01:57:18 +0100

    deleted obj:

---
+++ Objc/Sources/ErrorlessCrashReporter/ErrorlessCrashReporter.m
@@ -0,0 +1,12 @@
+//
+//  NSObject+TestFile.m
+//  
+//
+//  Created by Tigran on 08.01.23.
+//
+
+#import "NSObject+TestFile.h"
+
+@implementation NSObject (TestFile)
+
+@end
---
+++ Objc/Sources/ErrorlessCrashReporter/include/ErrorlessCrashReporter.h
@@ -0,0 +1,16 @@
+//
+//  NSObject+TestFile.h
+//  
+//
+//  Created by Tigran on 08.01.23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (TestFile)
+
+@end
+
+NS_ASSUME_NONNULL_END
