bb5e05a4f12c12933c9d3f4d6ae2e908c01696da
jantiks <58886410+jantiks@users.noreply.github.com>
2023-01-09 23:45:47 +0100

    bringing back the commented code

--- Swift/Sources/Private/CrashReporter/ErrorlessCrashReporter.swift
+++ Swift/Sources/Private/CrashReporter/ErrorlessCrashReporter.swift
@@ -51,12 +51,11 @@ struct ErrorlessCrashReporter {
     }
     
     private func amIBeingDebugged() -> Bool {
-        return true
-//        var info = kinfo_proc()
-//        var mib : [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
-//        var size = MemoryLayout.stride(ofValue: info)
-//        let junk = sysctl(&mib, UInt32(mib.count), &info, &size, nil, 0)
-//        assert(junk == 0, "sysctl failed")
-//        return (info.kp_proc.p_flag & P_TRACED) != 0
+        var info = kinfo_proc()
+        var mib : [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
+        var size = MemoryLayout.stride(ofValue: info)
+        let junk = sysctl(&mib, UInt32(mib.count), &info, &size, nil, 0)
+        assert(junk == 0, "sysctl failed")
+        return (info.kp_proc.p_flag & P_TRACED) != 0
     }
 }
