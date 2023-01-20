7bcb7f743265c4ed07b746a69eee257237b68a41
jantiks <58886410+jantiks@users.noreply.github.com>
2023-01-20 22:36:35 +0100

    fixed the endpoint

--- Swift/Sources/Private/CrashReporter/ErrorlessCrashReporter.swift
+++ Swift/Sources/Private/CrashReporter/ErrorlessCrashReporter.swift
@@ -52,7 +52,7 @@ struct ErrorlessCrashReporter {
     }
     
     private func postCrash(_ data: String?) {
-        var req = URLRequest(url: URL(string: "http://127.0.0.1:8showMessageshowMessage080showMessage/crash")!)
+        var req = URLRequest(url: URL(string: "http://127.0.0.1:8/crash")!)
         
         req.httpBody = try! JSONEncoder().encode(RequestBody(crashRepoort: data ?? ""))
         req.httpMethod = "POST"
