f2c3e79014bdc0bf69c3d387255ad82338fea19a
jantiks <58886410+jantiks@users.noreply.github.com>
2023-01-29 18:00:23 +0100

    added test case for the event tracker

--- Tests/ErrorlesSDKTests/ErrorlesSDKTests.swift
+++ Tests/ErrorlesSDKTests/ErrorlesSDKTests.swift
@@ -2,10 +2,20 @@ import XCTest
 @testable import ErrorlesSDK
 
 final class ErrorlesSDKTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(ErrorlesSDK().text, "Hello, World!")
+    func testTrackEvents() throws {
+        // Set expectation. Used to test async code.
+        let expectation = XCTestExpectation(description: "response")
+        
+        // Make mock network request to get profile
+        ErrorlessTracker().track(.viewDidLoad) { result in
+            switch result {
+            case .success(_):
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 3)
     }
 }
